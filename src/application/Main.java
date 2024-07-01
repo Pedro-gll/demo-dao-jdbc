a3c5676fb26c6d6ab5e147e742106833e92f19e2
Pedro Lima <ped7864@gmail.com>
2024-07-01 13:04:51 -0300

    DepartmentDao and SellerDao interfaces update

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,5 +11,6 @@ public class Main {
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(seller);
         System.out.println(obj);
+
     }
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,2 +1,13 @@
-package model.dao;public interface SellerDao {
+package model.dao;
+
+import model.entites.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller seller);
+    void update(Seller seller);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
 }
