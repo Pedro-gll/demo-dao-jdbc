9d7341bf2f0d9ffed8bfdbcd6a8f4fb000f314a5
Pedro Lima <ped7864@gmail.com>
2024-07-01 14:57:18 -0300

    findById implamentation

--- src/model/dao/daoFactory.java
+++ src/model/dao/daoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.SellerDao;
 import model.dao.impl.SellerDaoJDBC;
 
 public class daoFactory {
 
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
